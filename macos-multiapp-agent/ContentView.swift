9f6a923b299dd8234d6fe20f836935235d157eb2
Juan M. Ibarra <jmibarra86@gmail.com>
2026-03-12 18:47:33 -0300

    Initial Commit

---
+++ macos-multiapp-agent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macos-multiapp-agent
+//
+//  Created by Juan Ibarra on 12/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macos-multiapp-agent/macos_multiapp_agentApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_multiapp_agentApp.swift
+//  macos-multiapp-agent
+//
+//  Created by Juan Ibarra on 12/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct macos_multiapp_agentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
